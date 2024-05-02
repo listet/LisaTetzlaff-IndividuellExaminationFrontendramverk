3918e5ee6b7a3f88124618b75f75c81640104a4b
Lisa Tetzlaff <lisa.emma.tetzlaff@gmail.com>
2024-05-02 14:50:19 +0200

    events css

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import './event.css'
+import { Link } from "react-router-dom"
+// Use param here 
 
-function Event() {
+function Event({ data }) {
+
+    function formatDate(dateStr) {
+        const parts = dateStr.split(" "); // Delar upp "13 December" till ["13", "December"]
+        const day = parts[0]; // Dagen, "13"
+        const month = parts[1].slice(0, 3); // De första tre bokstäverna i månaden, "Dec"
+        return { day, month }; // Sätter ihop dem igen till "13 Dec"
+    }
+
+    const { day, month } = formatDate(data.when.date);
+
     return (
-        <div>
-
-        </div>
+        <section className="event-container">
+            <article className='event-date'>
+                <p className='day'>{day}</p>
+                <p className='month'>{month}</p>
+            </article>
+            <article className='event-info'>
+                <h2 className='event-title'>{data.name}</h2>
+                <p className='event-place'>{data.where}</p>
+                <p className='event-time'>{data.when.from} - {data.when.to}</p>
+            </article>
+            <p className='event-price'>{data.price}sek</p>
+        </section>
     )
 }
 
--- src/components/fetchEvents/FetchEvents.jsx
+++ src/components/fetchEvents/FetchEvents.jsx
@@ -1,32 +1,34 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Event from '../event/Event';
+import './fetchEvents.css'
 
 function FetchEvents() {
 
     const [events, setEvents] = useState([]);
 
     useEffect(() => {
-        // Function to fetch events when component mounts
         const getEvents = () => {
             axios.get(`https://santosnr6.github.io/Data/events.json`)
                 .then(response => {
-                    // Update state with the fetched events
-                    setEvents(response.data);
-                    console.log('Fetched events:', response.data);
+                    // Uppdaterar state
+                    setEvents(response.data.events);
+                    console.log('Fetched events:', response.data.events);
                 })
                 .catch(error => {
                     console.error('Error fetching events:', error);
                 });
-
-        };
+        }
 
         getEvents();
     }, []);
 
     return (
-        <>
-
-        </>
+        <section className='events'>
+            {events.map((event, index) => (
+                <Event key={index} data={event} />
+            ))}
+        </section>
     );
 }
 
