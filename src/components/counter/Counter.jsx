60b7c6af80061cf94960472a503b2267f630549a
Lisa Tetzlaff <lisa.emma.tetzlaff@gmail.com>
2024-05-07 15:05:55 +0200

    orders working

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import './counter.css'
 import useEventStore from '../../store/event-store';
 import { Link } from 'react-router-dom';
@@ -6,46 +5,29 @@ import Button from '../button/Button';
 
 function Counter({ event }) {
 
-    // const { price, name } = event;  // Nu kan du extrahera price och name direkt från event objektet
-    const [eventBalance, setEventBalance] = useState(0);
 
-    const { increaseQty, decreaseQty, setOrders, orders, setEvent } = useEventStore((state) => ({
-        increaseQty: state.increaseQty,
-        decreaseQty: state.decreaseQty,
+    const { setOrders, orders, setEvent } = useEventStore((state) => ({
         orders: state.orders,
         setOrders: state.setOrders,
         setEvent: state.setEvent,
     }));
 
-    // const increaseQty = useEventStore(state => state.increaseQty);
-    // const decreaseQty = useEventStore(state => state.decreaseQty);
-    // const addOrder = useEventStore(state => state.addOrder);
 
-    useEffect(() => {
-        // Set initial quantity to 0 when component mounts
-        setEventBalance(0);
-    }, [event.name]);
 
     const handleDecreaseEventBalance = () => {
         if (event.qty > 1) {
             const newQuantity = event.qty - 1;
             setEvent({ ...event, qty: newQuantity });
         }
-
-        // if (eventBalance > 0) {
-        //     decreaseQty(event.name);
-        //     setEventBalance(prev => prev - 1);
-        // }
     };
 
     const handleIncreaseEventBalance = () => {
         const newQuantity = event.qty + 1;
         setEvent({ ...event, qty: newQuantity });
-        // increaseQty(event.name);
-        // setEventBalance(prev => prev + 1);
     };
+
     const handleAddToCart = () => {
-        // const existingOrder = orders.find(order => order.name === event.name);
+
         const existingOrderIndex = orders.findIndex(order => order.id === event.id);
 
         if (existingOrderIndex !== -1) {
@@ -53,9 +35,7 @@ function Counter({ event }) {
             const orderCopy = [...orders];
             orderCopy[existingOrderIndex].qty = event.qty;
             // Om eventet redan finns, uppdatera endast quantity
-            // const updatedOrders = orders.map(order =>
-            //     order.name === event.name ? { ...order, qty: order.qty + qty } : order
-            // );
+
             setOrders(orderCopy);
         } else {
             // Om eventet inte finns, lägg till det nya eventet
--- src/store/event-store.js
+++ src/store/event-store.js
@@ -10,53 +10,58 @@ const useEventStore = create((set) => ({
         const eventsQty = newEvents.map(event => ({
             ...event, qty: 0
         }));
-        set({ events: eventsQty }) //cart:eventsQty?)
+        set({ events: eventsQty })
     },
     setEvent: (newEvent) => {
         set({ event: newEvent })
     },
     setOrders: (newOrder) => set({ orders: newOrder }),
-    increaseQty: (name) => {
-        set(state => ({
-            events: state.events.map(event => {
-                if (event.name === name) {
-                    return { ...event, qty: event.qty + 1 };
-                }
-                return event;
-            })
-        }));
-    },
-    decreaseQty: (name) => {
-        set(state => ({
-            events: state.events.map(event => {
-                if (event.name === name && event.qty > 0) {
-                    return { ...event, qty: event.qty - 1 };
-                }
-                return event;
-            })
-            // addOrder: (event, quantity) => set(state => {
-            //     console.log(event)
-            //     const existingOrder = state.orders.find(order => order.event.name === event.name);
-            //     if (existingOrder) {
-            //         console.log(state.orders)
-            //         // Om eventet redan finns, uppdatera endast quantity
-            //         const updatedOrders = state.orders.map(order =>
-            //             order.event.name === event.name ? { ...order, quantity: order.quantity + quantity } : order
-            //         );
-            //         return { orders: updatedOrders };
-            //     } else {
-            //         // Om eventet inte finns, lägg till det nya eventet
-            //         return { orders: [...state.orders, { event, quantity }] };
-            //     }
-            // }),
-        }));
-    },
     setPrice: (price) => set({ price }),
     clearEvents: () => set({ events: [] }),
     clearOrders: () => set({ orders: [] }),
 }));
 
 export default useEventStore;
+
+// increaseQty: (name) => {
+//     set(state => ({
+//         events: state.events.map(event => {
+//             if (event.name === name) {
+//                 return { ...event, qty: event.qty + 1 };
+//             }
+//             return event;
+//         })
+//     }));
+// },
+// decreaseQty: (name) => {
+//     set(state => ({
+//         events: state.events.map(event => {
+//             if (event.name === name && event.qty > 0) {
+//                 return { ...event, qty: event.qty - 1 };
+//             }
+//             return event;
+//         })
+// addOrder: (event, quantity) => set(state => {
+//     console.log(event)
+//     const existingOrder = state.orders.find(order => order.event.name === event.name);
+//     if (existingOrder) {
+//         console.log(state.orders)
+//         // Om eventet redan finns, uppdatera endast quantity
+//         const updatedOrders = state.orders.map(order =>
+//             order.event.name === event.name ? { ...order, quantity: order.quantity + quantity } : order
+//         );
+//         return { orders: updatedOrders };
+//     } else {
+//         // Om eventet inte finns, lägg till det nya eventet
+//         return { orders: [...state.orders, { event, quantity }] };
+//     }
+// }),
+//     }));
+// },
+//     setPrice: (price) => set({ price }),
+//     clearEvents: () => set({ events: [] }),
+//     clearOrders: () => set({ orders: [] }),
+// }));
 
 // addOrder: (event, quantity) => set(state => ({
 //     orders: [...state.orders, { event, quantity }]
